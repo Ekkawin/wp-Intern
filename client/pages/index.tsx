4292587337b5e0c5bd3433f4aff249580eae7803
Aek <ekkawin.v@gmail.com>
2020-07-27 15:42:11 +0700

    repair code from last commit

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -98,7 +98,7 @@ const SimpleBarChart = () => {
     >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis type="number" />
-      <YAxis dataKey="name" type="cate  gory" />
+      <YAxis dataKey="name" type="category" />
       <Tooltip />
       <Legend />
       <Bar dataKey="pv" fill="#8884d8" />
